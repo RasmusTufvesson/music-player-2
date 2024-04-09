f60274a40866aa1b7165ceedef61e1ba0cfbc8f8
RasmusTufvesson <lars@bizzbook.com>
2024-04-09 20:59:25 +0200

    add songs tab

--- src/app.rs
+++ src/app.rs
@@ -1,9 +1,15 @@
 use std::sync::mpsc::{Receiver, Sender, TryRecvError};
-use eframe::egui::{self, Vec2};
+use eframe::egui::{self, Label, RichText, Sense, Vec2};
 use crate::music::Packet;
 
 pub enum StatusPacket {
     NextSong,
+}
+
+#[derive(PartialEq)]
+enum Tab {
+    Controls,
+    Songs,
 }
 
 pub struct App {
@@ -14,6 +20,7 @@ pub struct App {
     paused: bool,
     songs: Vec<String>,
     index: usize,
+    tab: Tab,
 }
 
 impl App {
@@ -26,6 +33,7 @@ impl App {
             paused: true,
             songs,
             index: 0,
+            tab: Tab::Controls,
         };
     }
 }
@@ -52,30 +60,63 @@ impl eframe::App for App {
                 }
             }
         }
-        egui::SidePanel::left("side_panel").exact_width(20.).resizable(false).show(ctx, |ui| {
-            ui.add_space(ui.spacing().item_spacing.y);
-            ui.spacing_mut().slider_width = ui.available_height() - ui.spacing().item_spacing.y;
-            if ui.add(egui::Slider::new(&mut self.volume, 0.0..=1.5).show_value(false).vertical()).changed() {
-                self.sender.send(Packet::Volume(self.volume)).unwrap();
-            }
-        });
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.with_layout(egui::Layout { main_dir: egui::Direction::TopDown, main_wrap: false, main_align: eframe::emath::Align::Min, main_justify: false, cross_align: eframe::emath::Align::Center, cross_justify: true }, |ui: &mut egui::Ui| {
-                ui.heading(&self.songs[self.index]);
-                if ui.button(match self.paused {
-                    true => "Play",
-                    false => "Pause",
-                }).clicked() {
-                    self.sender.send(Packet::Pause).unwrap();
-                    self.paused = !self.paused;
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            egui::menu::bar(ui, |ui| {
+                if ui.button("Controls").clicked() {
+                    self.tab = Tab::Controls;
                 }
-                if ui.button("Skip").clicked() {
-                    self.sender.send(Packet::Skip).unwrap();
-                }
-                if ui.checkbox(&mut self.looping, "Loop").changed() {
-                    self.sender.send(Packet::Loop).unwrap();
+                if ui.button("Songs").clicked() {
+                    self.tab = Tab::Songs;
                 }
             });
+        });
+        if self.tab == Tab::Controls {
+            egui::SidePanel::left("side_panel").exact_width(20.).resizable(false).show(ctx, |ui| {
+                ui.add_space(ui.spacing().item_spacing.y);
+                ui.spacing_mut().slider_width = ui.available_height() - ui.spacing().item_spacing.y;
+                if ui.add(egui::Slider::new(&mut self.volume, 0.0..=1.5).show_value(false).vertical()).changed() {
+                    self.sender.send(Packet::Volume(self.volume)).unwrap();
+                }
+            });
+        }
+        egui::CentralPanel::default().show(ctx, |ui| {
+            match self.tab {
+                Tab::Controls => {
+                    ui.with_layout(egui::Layout { main_dir: egui::Direction::TopDown, main_wrap: false, main_align: eframe::emath::Align::Min, main_justify: false, cross_align: eframe::emath::Align::Center, cross_justify: true }, |ui: &mut egui::Ui| {
+                        ui.add(Label::new(RichText::new(&self.songs[self.index]).heading()).selectable(false).truncate(true));
+                        if ui.button(match self.paused {
+                            true => "Play",
+                            false => "Pause",
+                        }).clicked() {
+                            self.sender.send(Packet::Pause).unwrap();
+                            self.paused = !self.paused;
+                        }
+                        if ui.button("Skip").clicked() {
+                            self.sender.send(Packet::Skip).unwrap();
+                        }
+                        if ui.checkbox(&mut self.looping, "Loop").changed() {
+                            self.sender.send(Packet::Loop).unwrap();
+                        }
+                    });
+                }
+                Tab::Songs => {
+                    egui::ScrollArea::vertical().auto_shrink(false).show(ui, |ui| {
+                        egui::Grid::new("songs").show(ui, |ui| {
+                            for (i, song) in self.songs.iter().enumerate() {
+                                if i == self.index {
+                                    ui.add(Label::new(RichText::new(song).strong()).selectable(false));
+                                } else {
+                                    if ui.add(Label::new(song).sense(Sense::click()).selectable(false)).clicked() {
+                                        self.sender.send(Packet::Play(i)).unwrap();
+                                        self.index = i;
+                                    }
+                                }
+                                ui.end_row();
+                            }
+                        });
+                    });
+                }
+            }
         });
     }
 }
